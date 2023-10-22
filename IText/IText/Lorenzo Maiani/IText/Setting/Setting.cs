1e7a2c8bd7c17274ac742a9591817323164cb6d4
LorenzoMaiani0 <lorenzo.maiani3@studio.unibo.it>
2023-10-22 18:31:34 +0200

    Create project and start adding C# class

---
+++ IText/IText/Lorenzo Maiani/IText/Setting/Setting.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace IText.Lorenzo_Maiani.IText.Setting
+{
+    class Setting
+    {
+        private static Setting instance;
+
+        private Setting() 
+        {
+
+        }
+
+        public static Setting GetInstance()
+        {
+            if( instance == null)
+            {
+                instance = new Setting();
+            }
+            return instance;
+        }
+    }
+}
