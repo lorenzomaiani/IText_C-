f38e4dc4b4a52305eb371284204fdf67d9304b23
LorenzoMaiani0 <lorenzo.maiani3@studio.unibo.it>
2023-10-24 18:38:52 +0200

    Set LF as standard

--- IText/IText/Lorenzo Maiani/Test/Test.cs
+++ IText/IText/Lorenzo Maiani/Test/Test.cs
@@ -1,8 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using IText.Lorenzo_Maiani.IText.File;
+using IText.Lorenzo_Maiani.IText.SaveFileController;
+using System;
 
 namespace IText.Lorenzo_Maiani.Test
 {
@@ -10,14 +8,34 @@ namespace IText.Lorenzo_Maiani.Test
     {
         public static void Main(string[] args)
         {
-           
-            
+            OpenFileDialog dialog = new OpenFileDialog();
+
+
         }
 
-        private void Close(string mess)
+        private void Test1()
         {
-            Console.WriteLine("Premere un tasto per continuare...");
-            Console.Read();
+            Console.WriteLine("Inserire il path del file");
+            string path = Console.ReadLine();
+            Console.WriteLine("Inserire il nome del file");
+            string name = Console.ReadLine();
+
+            FileModel file = new FileModel(path,name);
+
+            SaveFileController saveFileController = new SaveFileController(file.GetFilePath(), file.GetFileName());
+            saveFileController.CreateAFile();
+            string text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, " + "\n "+
+                "sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut " + "\n " +
+                "enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut " + "\n " +
+                "aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in " + "\n " +
+                "voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint" + "\n " +
+                " occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit" + "\n " +
+                " anim id est laborum.";
+
+            saveFileController.SaveOnFile(text);
+
         }
+
+        
     }
 }
